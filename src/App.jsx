cd0488e9d05aed934cbaea63a61dc4d254f7618f
Yun Gu <guy36@cardiff.ac.uk>
2022-02-21 20:45:24 +0000

    palette changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Intro from './components/intro/Intro'
 import MyFooter from './components/myFooter/MyFooter'
 import Toggle from './components/toggle/Toggle'
 import { ThemeContext } from './context'
-import { useContext } from "react";
+import { useContext } from "react"
 
 
 const App = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
-  
+
   return (
     <div style={{backgroundColor:darkMode?"#222":"#fff", color:darkMode?"#fff":"#222"}}>
       <Toggle/>
--- src/components/Contract/Contract.jsx
+++ src/components/Contract/Contract.jsx
@@ -33,16 +33,16 @@ const Contract = () => {
                     </div>
                     <div className="c-info-icons">
                         <a href="mailto:GuY36@cardiff.ac.uk" t>
-                            <FaEnvelopeSquare className="c-info-icon" style={{color:darkMode?"#b9ac92":"#000"}}/>
+                            <FaEnvelopeSquare className="c-info-icon" style={{color:darkMode?"#fff":"#000"}}/>
                         </a>
                         <a href="https://www.instagram.com/yyyun_k/">
-                            <FaInstagramSquare className="c-info-icon" style={{color:darkMode?"#b9ac92":"#000"}}/>
+                            <FaInstagramSquare className="c-info-icon" style={{color:darkMode?"#fff":"#000"}}/>
                         </a>
                         <a href="https://github.com/YunaGu">
-                            <FaGithubSquare className="c-info-icon" style={{color:darkMode?"#b9ac92":"#000"}}/>
+                            <FaGithubSquare className="c-info-icon" style={{color:darkMode?"#fff":"#000"}}/>
                         </a>
                         <a href="https://www.linkedin.com/in/yun-gu-0932b11a5/">
-                            <FaLinkedin className="c-info-icon" style={{color:darkMode?"#b9ac92":"#000"}}/>
+                            <FaLinkedin className="c-info-icon" style={{color:darkMode?"#fff":"#000"}}/>
                         </a>
                     </div>
                   </div>
@@ -50,10 +50,10 @@ const Contract = () => {
               <div className="c-right">
                   <p className="c-desc"><strong>Do you have any questions or suggestions?</strong> Always feel free to contract me, please.</p>
                   <form ref={formRef} onSubmit={handleSubmit}>
-                      <input style={{backgroundColor:darkMode?"#b9ac92":"#fff"}} type="text" placeholder="Your name" name="user_name" required/>
-                      <input style={{backgroundColor:darkMode?"#b9ac92":"#fff"}} type="text" placeholder="Subject" name="user_subject" required/>
-                      <input style={{backgroundColor:darkMode?"#b9ac92":"#fff"}} type="text" placeholder="Your Email" name="user_email" required/>
-                      <textarea style={{backgroundColor:darkMode?"#b9ac92":"#fff"}} name="message"  placeholder="Message" rows="5" required></textarea>
+                      <input style={{backgroundColor:darkMode?"#333":"#fff"}} type="text" placeholder="Your name" name="user_name" required/>
+                      <input style={{backgroundColor:darkMode?"#333":"#fff"}} type="text" placeholder="Subject" name="user_subject" required/>
+                      <input style={{backgroundColor:darkMode?"#333":"#fff"}} type="text" placeholder="Your Email" name="user_email" required/>
+                      <textarea style={{backgroundColor:darkMode?"#333":"#fff"}} name="message"  placeholder="Message" rows="5" required></textarea>
                       <button>Submit</button>
                       {done && "Thank you..."}
                   </form>
